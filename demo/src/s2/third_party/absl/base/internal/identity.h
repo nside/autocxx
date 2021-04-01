69a15e501f26840b64e505bc0c2b02ad15871874
Denis <denis@2ni.net>
2021-03-31 19:25:15 -0700

    Repro static methods lookup bug

--- demo/src/s2/third_party/absl/base/internal/identity.h
+++ demo/src/s2/third_party/absl/base/internal/identity.h
@@ -1,29 +1,33 @@
-// Copyright 2020 Google LLC
+// Copyright 2017 The Abseil Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//    https://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
 
-use autocxx::include_cpp;
+#ifndef S2_THIRD_PARTY_ABSL_BASE_INTERNAL_IDENTITY_H_
+#define S2_THIRD_PARTY_ABSL_BASE_INTERNAL_IDENTITY_H_
 
-include_cpp! {
-    #include "s2/util/math/vector.h"
+namespace absl {
+namespace internal {
 
-    #include "s2/r2rect.h"
-    safety!(unsafe_ffi)
-    generate!("R2Rect")
-    generate!("R1Interval")
-}
+template <typename T>
+struct identity {
+  typedef T type;
+};
 
-fn main() {
-    let r = ffi::Rect::Empty();
-    println!("Hello, world!");
-}
+template <typename T>
+using identity_t = typename identity<T>::type;
+
+}  // namespace internal
+}  // namespace absl
+
+#endif  // S2_THIRD_PARTY_ABSL_BASE_INTERNAL_IDENTITY_H_
--- demo/src/s2/util/gtl/layout.h
+++ demo/src/s2/util/gtl/layout.h
@@ -1,29 +1,28 @@
-// Copyright 2020 Google LLC
+// Copyright Google Inc. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//    https://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS-IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
 
-use autocxx::include_cpp;
+#ifndef S2_UTIL_GTL_LAYOUT_H_
+#define S2_UTIL_GTL_LAYOUT_H_
 
-include_cpp! {
-    #include "s2/util/math/vector.h"
+#include "s2/third_party/absl/container/internal/layout.h"
 
-    #include "s2/r2rect.h"
-    safety!(unsafe_ffi)
-    generate!("R2Rect")
-    generate!("R1Interval")
-}
+namespace gtl {
 
-fn main() {
-    let r = ffi::Rect::Empty();
-    println!("Hello, world!");
-}
+using absl::container_internal::Aligned;
+using absl::container_internal::Layout;
+
+}  // namespace gtl
+
+#endif  // S2_UTIL_GTL_LAYOUT_H_
